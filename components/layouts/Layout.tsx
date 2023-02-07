fdd6966068fdd7d0afadbb9fd5126dd099be1e30
John Elmo Johnson <elmoj6@gmail.com>
2023-02-07 16:19:48 +1100

    Fixed drawer bug when mobile

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -13,20 +13,21 @@ const Layout: React.FC<{ children: any }> = ({ children }) => {
   const { isMobileOpen, handleDrawerToggle } = useContext(AppContext);
   return (
     <Box sx={{ display: "flex", flexDirection: "row" }}>
-      <Drawer sx={{ display: { sm: "none", md: "flex" } }} variant="permanent">
+      <Drawer sx={{ display: { xs: "none", md: "flex" } }} variant="permanent" open>
         <AppDrawer />
       </Drawer>
       <Drawer
         open={isMobileOpen}
         onClose={handleDrawerToggle}
-        sx={{ display: { sm: "flex", md: "none" } }}
+        sx={{ display: { xs: "flex", md: "none" } }}
+        variant="temporary"
       >
         <AppDrawer />
       </Drawer>
       <Box
         sx={{
-          width: { sm: "100%", md: "calc(100% - 250px)" },
-          ml: { sm: "0px", md: "250px" },
+          width: { xs: "100%", md: "calc(100% - 250px)" },
+          ml: { xs: "0px", md: "250px" },
         }}
       >
         <Navbar />
