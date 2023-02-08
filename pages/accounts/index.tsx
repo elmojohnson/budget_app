4b9cde22d261576ededce99769e9b7a710cb33e7
John Elmo Johnson <elmoj6@gmail.com>
2023-02-08 14:36:59 +1100

    Update: Accounts page set-up

--- pages/accounts/index.tsx
+++ pages/accounts/index.tsx
@@ -1,8 +1,35 @@
+import { useRouter } from "next/router";
+
 import Layout from "@/components/layouts/Layout";
-import React from "react";
+
+// MUI
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const Accounts = () => {
-  return <Layout>Accounts</Layout>;
+  const router = useRouter();
+  return (
+    <Layout>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h5">Accounts</Typography>
+        <Button
+          variant="contained"
+          onClick={() =>
+            router.push("/accounts/create", undefined, { shallow: true })
+          }
+        >
+          Create
+        </Button>
+      </Box>
+    </Layout>
+  );
 };
 
 export default Accounts;
