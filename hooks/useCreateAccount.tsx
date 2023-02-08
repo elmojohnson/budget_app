03d3b5887cd68e2437a6509bd4549a4064f283b7
John Elmo Johnson <elmoj6@gmail.com>
2023-02-08 14:41:28 +1100

    Prepared pages, hooks, and form for create account

---
+++ hooks/useCreateAccount.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useCreateAccount = () => {
+  return (
+    <div>useCreateAccount</div>
+  )
+}
+
+export default useCreateAccount
--- pages/accounts/create.tsx
+++ pages/accounts/create.tsx
@@ -1,9 +1,16 @@
-import Layout from '@/components/layouts/Layout'
+import AccountForm from "@/components/forms/AccountForm";
+import Layout from "@/components/layouts/Layout";
+
+// MUI
+import Typography from "@mui/material/Typography";
 
 const CreateAccount = () => {
   return (
-    <Layout>CreateAccount</Layout>
-  )
-}
+    <Layout>
+      <Typography variant="h5">Create an account</Typography>
+      <AccountForm />
+    </Layout>
+  );
+};
 
-export default CreateAccount
+export default CreateAccount;
