d46f54a43c368c48fc5ca7a54469a94c306b2264
John Elmo Johnson <elmoj6@gmail.com>
2023-02-08 14:46:38 +1100

    Update: changed naming to snake case

--- validation/AccountSchema.ts
+++ validation/AccountSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 const AccountSchema = Yup.object().shape({
   name: Yup.string().max(100, "Name is too long").required("Please enter account name"),
-  initialBalance: Yup.number().required("Please enter initial balance"),
+  initial_balance: Yup.number().required("Please enter initial balance"),
   notes: Yup.string(),
 });
 
